e26242a0a473f80fec254241ed8944642fd46db4
Simileoluwa aluko <simialuko24@gmail.com>
2022-05-04 18:41:35 +0100

    trying to create continous time series

--- lib/SFChart.dart
+++ lib/SFChart.dart
@@ -71,6 +71,8 @@ class TimeSeries {
 }
 */
 
+import 'dart:async';
+
 import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -87,16 +89,28 @@ class ChartApp extends StatefulWidget {
 
 class ChartAppState extends State<ChartApp> {
   List<TimeSeries> pressureData = <TimeSeries>[];
+  List<TimeSeries> pressureChartData = <TimeSeries>[];
+  int pressureDataCount = 0;
+
   List<TimeSeries> flowData = <TimeSeries>[];
+  List<TimeSeries> flowChartData = <TimeSeries>[];
+  int flowDataCount = 0;
+
+  Timer? timer;
+  ChartSeriesController? _chartSeriesController;
+
+  int flowGraphStartIndex = 0;
+  int flowGraphEndIndex = 19;
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: loadAsset(),
       builder: (ctx, snapshot) {
-
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator(),);
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         }
 
         return SfCartesianChart(
@@ -106,8 +120,8 @@ class ChartAppState extends State<ChartApp> {
                 majorGridLines: MajorGridLines(width: 0),
                 edgeLabelPlacement: EdgeLabelPlacement.shift,
                 intervalType: DateTimeIntervalType.seconds,
-                interval: 8,
-                labelRotation: 120,
+                // interval: 5,
+                // labelRotation: 60,
                 axisLabelFormatter: (AxisLabelRenderDetails args) {
                   late String text;
                   text = DateTime.fromMillisecondsSinceEpoch(args.value.toInt())
@@ -125,12 +139,20 @@ class ChartAppState extends State<ChartApp> {
                 }),
             series: <ChartSeries<TimeSeries, DateTime>>[
               LineSeries<TimeSeries, DateTime>(
+                onRendererCreated: (ChartSeriesController controller) {
+                  // Assigning the controller to the _chartSeriesController.
+                  _chartSeriesController = controller;
+                },
                 dataSource: flowData,
                 xValueMapper: (TimeSeries sales, _) => sales.time,
                 yValueMapper: (TimeSeries sales, _) => sales.parameter,
                 name: 'Sales',
               ),
               LineSeries<TimeSeries, DateTime>(
+                onRendererCreated: (ChartSeriesController controller) {
+                  // Assigning the controller to the _chartSeriesController.
+                  _chartSeriesController = controller;
+                },
                 dataSource: pressureData,
                 xValueMapper: (TimeSeries sales, _) => sales.time,
                 yValueMapper: (TimeSeries sales, _) => sales.parameter,
@@ -141,14 +163,32 @@ class ChartAppState extends State<ChartApp> {
     );
   }
 
+  void _updateDataSource(Timer timer) {
+    print("calling update");
+    if (flowData.isNotEmpty) {
+      print("flow not empty");
+      flowChartData.add(flowData[flowChartData.length]);
+      if (flowChartData.length == 21) {
+        // Removes the last index data of data source.
+        flowChartData.removeAt(0);
+        // Here calling updateDataSource method with addedDataIndexes to add data in last index and removedDataIndexes to remove data from the last.
+        _chartSeriesController?.updateDataSource(
+            addedDataIndexes: <int>[flowChartData.length - 1],
+            removedDataIndexes: <int>[0]);
+      }
+    }
+  }
+
   loadAsset() async {
     final myData = await rootBundle.loadString('assets/test_data.csv');
     List<List<dynamic>> rowsAsListOfValues =
         const CsvToListConverter().convert(myData);
     var data1 = <TimeSeries>[];
     var data2 = <TimeSeries>[];
+    var chartdata1 = <TimeSeries>[];
+    var chartdata2 = <TimeSeries>[];
 
-    for (int i = 0; i < 20 /*rowsAsListOfValues.length*/; i++) {
+    for (int i = 0; i < rowsAsListOfValues.length; i++) {
       if (i == 0) continue;
 
       String dateString = rowsAsListOfValues[i][1];
@@ -158,12 +198,24 @@ class ChartAppState extends State<ChartApp> {
       // GraphDataClass channel2 = GraphDataClass(rowsAsListOfValues[i][1], rowsAsListOfValues[i][20], rowsAsListOfValues[i][21]);
       data1.add(TimeSeries(date, rowsAsListOfValues[i][6]));
       data2.add(TimeSeries(date, rowsAsListOfValues[i][7]));
+      if (i < 20) {
+        chartdata1.add(data1[i]);
+        chartdata2.add(data2[i]);
+      }
     }
 
     // setState(() {
-      flowData = data1;
-      pressureData = data2;
+    flowData = data1;
+    flowDataCount = flowData.length;
+    flowChartData = chartdata1;
+
+    pressureData = data2;
+    pressureDataCount = pressureData.length;
+    pressureChartData = chartdata2;
     // });
+
+    timer =
+        Timer.periodic(const Duration(milliseconds: 1000), _updateDataSource);
   }
 
   @override
